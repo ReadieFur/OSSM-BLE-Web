56285dff738b9ce5b124dba548dcd4732a36c083
Chloe Read <dev@readiefur.com>
2026-09-24 01:57:38 +0100

    feat: setLed

--- src/OssmBleClient.ts
+++ src/OssmBleClient.ts
@@ -21,4 +21,16 @@ export class OssmBleClient extends RadBleApi {
     constructor(device: BluetoothDevice) {
         super(OSSM_SERVICE_UUID, device);
     }
+
+    /**
+     * Set the OSSM LED indicator to a specific color
+     * @param r The red component of the color (0-255)
+     * @param g The green component of the color (0-255)
+     * @param b The blue component of the color (0-255)
+     * @note The firmware seems to reset this value after a couple hundred milliseconds
+     */
+    async setLed(r: number, g: number, b: number): Promise<void> {
+        // Range validation will be left to the firmware
+        await this.send({ op: "indicator.set", path: "indicator.status", args: { r, g, b } }, this.lease!);
+    }
 }
